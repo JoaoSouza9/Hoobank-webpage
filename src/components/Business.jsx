38029914e526a9a647f065261daacdfd9151690a
JoaoSouza9 <joaovictor.souza98@yahoo.com.br>
2022-09-28 18:24:22 -0300

    made the billing, business, feedbacks and stats sections

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -4,10 +4,28 @@ import Button from "./Button";
 import styles, { layout } from '../style';
 
 
+const FeatureCard = ({icon, title,content, index}) => {
+  return (
+    <div className={` flex flex-row p-6 rounded-[20px] ${index !== features.length -1 ? "mb-6" : "mb-0"} feature-card`}>
+      <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+        <img src={icon} alt="icon" />
+      </div>
+      <div className='flex-1 flex flex-col ml-3'>
+        <h4 className=' font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1'>
+          {title}
+        </h4>
+        <p className=' font-poppins font-normal text-dimWhite text-[16px] leading-[24px] mb-1'>
+          {content}
+        </p>
+      </div>
+    </div>
+  )
+}
+
+
 const Business = () => {
   return (
     <section id="features" className={layout.section}>
-      <div>
         <div className={layout.sectionInfo}>
           <h2 className={styles.heading2}>
           You do the business, <br className='sm:block hidden'/> we’ll handle the money.
@@ -17,9 +35,12 @@ const Business = () => {
           </p>
           <Button styles='mt-10'/>
         </div>
-      </div>
-      <div>
-
+      <div className={` ${layout.sectionImg} flex-col`}>
+        {features.map((feature, index) => {
+          return(
+            <FeatureCard key={feature.id} {...feature} index={index}/>
+          )
+        })}
       </div>
     </section>
     
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { feedback } from '../constants';
+import styles, { layout } from '../style';
+import Feedbackcard from './Feedbackcard';
+
 
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
+    <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
+      <div className='absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient'/>
+      <div className='w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]'>
+        <h1 className={styles.heading2}>
+        What people are <br className='sm:block hidden'/> saying about us.
+        </h1>
+        <div className='w-full md:mt-o mt-6'>
+          <p className={`${styles.paragraph} max-w-[450px] text-left`}>
+            Everything you need to accept card payments and grow your business anywhere on the planet.
+          </p>
+        </div>
+      </div>
+      <div className=' flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]'>
+        {feedback.map((feedback, index) => {
+          return(
+            <Feedbackcard key={feedback.id} {...feedback}/>
+          )
+        })}
+      </div>
+    </section>
+    
   )
 }
 
